4620fd6eed5a23be4164118d91520e9e8f96009b
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-09-15 18:29:38 -0300

    Add a maven hello world test  project

--- java/hello/src/main/java/com/github/fabriciofx/github/workflows/HelloWorld.java
+++ java/hello/src/main/java/com/github/fabriciofx/github/workflows/HelloWorld.java
@@ -1,8 +1,14 @@
-/**
+/*
  * SPDX-FileCopyrightText: Copyright (C) 2025 Fabrício Barros Cabral
  * SPDX-License-Identifier: MIT
-*/
+ */
+package com.github.fabriciofx.github.workflows;
 
+/**
+ * A simple Hello World!
+ *
+ * @since 0.0.1
+ */
 public final class HelloWorld {
     public static void main(final String[] args) {
         System.out.println("Hello World!");
