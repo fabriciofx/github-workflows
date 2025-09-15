5e5286328b3603040eca23f313fdfb4c25d85af9
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-09-14 23:43:10 -0300

    Add copyright header to test bash

--- bash/tests/hello.sh
+++ bash/tests/hello.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# SPDX-FileCopyrightText: Copyright (C) 2025 Fabrício Barros Cabral
+# SPDX-License-Identifier: MIT
+
+set -euo pipefail {0}
+
 name="Fabrício"
 
 for i in $(seq 1 10); do
