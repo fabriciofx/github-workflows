fcdc85ec2a7556b777a96dcbbbcd14435bd66339
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-09-15 18:02:26 -0300

    Add a hello world test for each language

---
+++ c-cpp/test/hello.c
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: Copyright (C) 2025 Fabrício Barros Cabral
+ * SPDX-License-Identifier: MIT
+ */
+#include <stdio.h>
+
+int
+main(void)
+{
+    printf("Hello World!\n");
+    return 0;
+}
---
+++ ts/test/hello.ts
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Copyright (C) 2025 Fabrício Barros Cabral
+// SPDX-License-Identifier: MIT
+
+function helloWorld(): void {
+    console.log("Hello, World!");
+}
+
+helloWorld();
