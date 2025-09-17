91b52f9061978da73063074e675bfda3cc64366f
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-09-17 09:59:47 -0300

    Add autotools to c-cpp project

---
+++ c-cpp/autogen.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: Copyright (C) 2025 Fabrício Barros Cabral
+# SPDX-License-Identifier: MIT
+
+set -e
+
+OS=$(uname | tr '[:upper:]' '[:lower:]')
+
+# macOS uses glibtoolize
+if [ "$OS" = "darwin" ]; then
+    LIBTOOLIZE=glibtoolize
+else
+    LIBTOOLIZE=libtoolize
+fi
+
+clean() {
+    echo -n "Cleaning created files... "
+    rm -rf autom4te.cache aclocal.m4 Makefile.in src/Makefile.in m4 \
+        src/.deps src/.libs configure configure~ config.status \
+        config.log libtool Makefile src/Makefile src/*.o src/.depend .depend \
+        config.h.in* config.h stamp-h1 src/hello src/hello-cpp build \
+        hello*.tar.gz
+    echo "done."
+}
+
+dist() {
+    echo -n "Cleaning temporary files... "
+    rm -rf autom4te.cache aclocal.m4 src/.deps src/.libs config.status \
+        config.log libtool Makefile src/Makefile src/*.o src/.depend .depend \
+        config.h.in~ configure~ config.h stamp-h1 src/hello src/hello-cpp \
+        hello*.tar.gz
+    echo "done."
+}
+
+build() {
+    echo "Creating configure and support files... "
+    # Create config.h.in
+    autoheader
+    # Create libtool files
+    $LIBTOOLIZE --force --copy
+    # Create aclocal macros
+    aclocal
+    # Create configure
+    autoconf
+    # Create Makefile.in
+    automake --add-missing --copy
+    echo "done."
+}
+
+case "$1" in
+    clean)
+        clean
+        ;;
+    build)
+        build
+        ;;
+    dist)
+        dist
+        ;;
+    *)
+        clean
+        build
+        ;;
+esac
